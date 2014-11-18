35de42bf0e183404e6e757bfa4e39528492c979f
paulyiend <paul@timble.net>
2014-11-18 11:03:05 +0000

    re #447 - upgrading site instance of ckeditor

--- component/ckeditor/resources/assets/ckeditor/styles.js
+++ component/ckeditor/resources/assets/ckeditor/styles.js
@@ -1,6 +1,6 @@
 ﻿/**
- * Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+ * Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 // This file contains style definitions that can be used by CKEditor plugins.
@@ -14,22 +14,22 @@
 // website requirements and design properly.
 
 CKEDITOR.stylesSet.add( 'default', [
-
 	/* Block Styles */
 
 	// These styles are already available in the "Format" combo ("format" plugin),
 	// so they are not needed here by default. You may enable them to avoid
 	// placing the "Format" combo in the toolbar, maintaining the same features.
+	/*
 	{ name: 'Paragraph',		element: 'p' },
-	//{ name: 'Heading 1',		element: 'h1' },
+	{ name: 'Heading 1',		element: 'h1' },
 	{ name: 'Heading 2',		element: 'h2' },
 	{ name: 'Heading 3',		element: 'h3' },
-	/*
 	{ name: 'Heading 4',		element: 'h4' },
 	{ name: 'Heading 5',		element: 'h5' },
 	{ name: 'Heading 6',		element: 'h6' },
 	{ name: 'Preformatted Text',element: 'pre' },
 	{ name: 'Address',			element: 'address' },
+	*/
 
 	{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
 	{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
@@ -55,6 +55,7 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: 'Strikethrough',	element: 'strike' },
 	{ name: 'Subscript',		element: 'sub' },
 	{ name: 'Superscript',		element: 'sup' },
+	*/
 
 	{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
 
@@ -77,7 +78,7 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: 'Language: LTR',	element: 'span', attributes: { 'dir': 'ltr' } },
 
 	/* Object Styles */
-    /*
+
 	{
 		name: 'Styled image (left)',
 		element: 'img',
@@ -106,6 +107,5 @@ CKEDITOR.stylesSet.add( 'default', [
 
 	{ name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
 	{ name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } }
-	*/
-]);
+] );
 
